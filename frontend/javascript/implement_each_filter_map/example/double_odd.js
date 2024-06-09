2f3d53ee0bc0b87f2a153b8360bce059f5f88af3
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-06-08 21:17:55 -0300

    js: simple arrow function example

---
+++ frontend/javascript/implement_each_filter_map/example/double_odd.js
@@ -0,0 +1,35 @@
+const doubleOdd = (arr) =>
+  arr.map((num) => {
+    if (num % 2 != 0) {
+      return num * 2;
+    } else return num;
+  });
+
+testSets = [
+  [
+    [2, 6],
+    [2, 6],
+  ],
+  [
+    [2, 3, 3, 5, 7, 10],
+    [2, 6, 6, 10, 14, 10],
+  ],
+  [
+    [7, 1432, 12, 13, 107],
+    [14, 1432, 12, 26, 214],
+    [[], []],
+  ],
+];
+
+function codeTest(testSets) {
+  for (const test of testSets) {
+    const doubleArr = doubleOdd(test[0]);
+    for (let i = 0; i <= doubleArr.length; i++)
+      if (doubleArr[i] != test[1][i]) {
+        return "Your code fails. Try again!";
+      }
+  }
+  return "Your code passes!";
+}
+
+console.log(codeTest(testSets));
